7152674f75f7d12a29deb441e25e7b5014e6739f
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-04 11:09:33 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import builtins
+import hidden_4
+
 if __name__ == "__main__":
-    for name in dir(builtins):
+    names = dir(hidden_4)
+    for name in names:
         if name[:2] != "__":
             print(name)
