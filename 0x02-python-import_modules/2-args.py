866be4fead89fd1fd4becd49eb9406cfa0ec7ac0
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-04 10:31:35 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 import sys
-print("{} arguments:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{}".format(sys.argv[i]))
+
+args_count = len(sys.argv) - 1
+if args_count == 0:
+    print("0 arguments.")
+else:
+    if args_count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args_count))
+
+    for i in range(args_count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
