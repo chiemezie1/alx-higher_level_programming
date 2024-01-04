ed8ccf8dbe3e3419d182226df71b9bb1762a6614
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-04 02:14:35 +0100

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 97, -1):
-    if (i - 122) % 2 == 0:
-        print("{}".format(chr(i)), end="")
+for i in reversed(range(97, 123)):
+    if (i % 2) == 0:
+        print("{:c}".format(i), end="")
     else:
-        print("{}".format(chr(i - 32)), end="")
+        print("{:c}".format(i - 32), end="")
