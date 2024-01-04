e7377459c42e730915c34d825d75be6875e5c832
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-04 10:45:44 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import sys
-
-args_count = len(sys.argv) - 1
-if args_count == 0:
-    print("{:d} arguments.".format(args_count))
-else:
-    if args_count == 1:
-        print("{:d} argument:".format(args_count))
-        print("{}: {}".format(args_count, sys.argv[args_count]))
+if __name__ == "__main__":
+    args_count = len(sys.argv) - 1
+    if args_count == 0:
+        print("{:d} arguments.".format(args_count))
     else:
-        print ("{:d} arguments:".format(args_count))
-        for i in range(args_count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        if args_count == 1:
+            print("{:d} argument:".format(args_count))
+            print("{}: {}".format(args_count, sys.argv[args_count]))
+        else:
+            print("{:d} arguments:".format(args_count))
+            for i in range(args_count):
+                print("{}: {}".format(i + 1, sys.argv[i + 1]))
