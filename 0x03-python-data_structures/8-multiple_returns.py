10aefb9798d2157915cd818082d77b00eac593e9
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-05 18:31:10 +0100

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length == 0:
+    if not sentence:
         first = None
     else:
         first = sentence[0]
-    return ('Length: {} - First character: {}'.format(length, first))
+    return (length, first)
