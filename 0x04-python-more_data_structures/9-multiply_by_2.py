0d74478712bebb89ceb4f8af04f54ad3719a72a3
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-11 15:14:34 +0100

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    b_dictionary = a_dictionary.copy()
+    for k, v in b_dictionary.items():
+        b_dictionary[k] = 2 * v
+    return b_dictionary
