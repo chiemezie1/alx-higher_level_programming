10a11bb176cfe9af54154f43bdca47cdcbac4e92
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-11 16:39:24 +0100

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
+    
     numerals_dict = {
         'I': 1,
         'V': 5,
@@ -11,12 +12,28 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
+    
     result = 0
+    
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and \
-                numerals_dict[roman_string[i]] < \
-                numerals_dict[roman_string[i + 1]]:
-            result -= numerals_dict[roman_string[i]]
+        if i > 0 and numerals_dict[roman_string[i]] > numerals_dict[roman_string[i - 1]]:
+            result += numerals_dict[roman_string[i]] - 2 * numerals_dict[roman_string[i - 1]]
         else:
             result += numerals_dict[roman_string[i]]
+    
     return result
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
