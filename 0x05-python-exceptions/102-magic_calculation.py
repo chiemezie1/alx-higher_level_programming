eaac8fd71176467cd56b5e456434262a440c4f5f
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-23 06:32:37 +0100

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > len(a):
+                raise Exception('Too far')
+
+            result += (a[i] ** b[i]) / i
+
+        except Exception:
+            result += a[i] + b[i]
+
+    return result
