c5cacf65bfcc7b67fe5b3080b6c1b4eb1f6f78fd
chiemezie1 <chiemezieagbo1@gmail.com>
2024-01-24 00:24:11 +0100

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,13 +5,15 @@ A class that defines a square
 
 """
 
+
 class Square:
     def __init__(self, size):
         """__init___
-        
+
         __init__ initializes the square
 
         Attributes:
             size (int): size of the square
         """
+
         self.__size = size
