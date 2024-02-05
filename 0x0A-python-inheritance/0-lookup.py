378db70980745253ef4169d2d6014e921632bbb4
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-05 17:47:28 +0100

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""""lookup module
+
+Returns a list of available attributes and methods of an object
+"""
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object"""
+    return dir(obj)
