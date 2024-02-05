f6bc8ca3003dd816025c6ecad34c3e0e24baa40f
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-05 17:58:37 +0100

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""""my_list module
+
+inherits from list
+
+"""
+
+
+class MyList(list):
+    """my_list class
+
+    inherits from list
+
+    Public instance method: print_sorted
+    
+    """
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
