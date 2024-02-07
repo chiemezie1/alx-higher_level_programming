09982c1225226aa88127cdd6d338e79473e40897
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-07 16:41:29 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,8 +11,11 @@ and save them to a file
 
 if __name__ == "__main__":
 
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    to_json = __import__('5-save_to_json_file')
+    from_json = __import__('6-load_from_json_file')
+
+    save_to_json_file = to_json.save_to_json_file
+    load_from_json_file = from_json.load_from_json_file
     my_list = load_from_json_file("add_item.json")
 
     my_list = []
