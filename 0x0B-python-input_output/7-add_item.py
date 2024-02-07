69b7b909323d147480afeb06c3ec64da77e2713e
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-07 17:26:44 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,15 +10,16 @@ and save them to a file
 
 
 if __name__ == "__main__":
-
     to_json = __import__('5-save_to_json_file')
     from_json = __import__('6-load_from_json_file')
 
     save_to_json_file = to_json.save_to_json_file
     load_from_json_file = from_json.load_from_json_file
-    my_list = load_from_json_file("add_item.json")
 
-    my_list = []
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
     save_to_json_file(my_list, "add_item.json")
