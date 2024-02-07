976418db6261b8602fc280b31f77bf3e64b7d762
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-07 15:09:33 +0100

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" importing Json """
+import json
+
+"""
+function that returns python object
+represented as a JSON object 
+"""
+
+
+def from_json_string(my_str):
+    """
+    returns the JSON representation of an object
+    """
+    return json.loads(my_str)
