73e782c409aac493b0be1f8fc895066cfe9c876e
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-07 17:49:03 +0100

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ function that returns the dictionary description
+ with simple data structure for JSON serialization of an object
+
+"""
+def class_to_json(obj):
+      """
+      returns the dictionary description
+      with simple data structure for JSON
+      serialization of an object
+      """
+      return obj.__dict__
