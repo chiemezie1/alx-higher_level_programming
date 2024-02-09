7881449a53007dc8828c0571432b9a84a0b969dc
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-09 13:47:24 +0100

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+class Rectangle that inherits from Base
+"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.check_integers_value(width, "width")
+        self.check_integers_value(height, "height")
+        self.check_integers_value(x, "x")
+        self.check_integers_value(y, "y")
+        
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        self.check_integers_value(value, "width")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        self.check_integers_value(value, "height")
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        x getter
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        x setter
+        """
+        self.check_integers_value(value, "x")
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        y getter
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        y setter
+        """
+        self.check_integers_value(value, "y")
+        self.__y = value
+
+    def check_integers_value(self, value, param):
+        """
+        check if value is an integer and > 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"{param} must be an integer")
+
+        if value <= 0 and param in ('width', 'height'):
+            raise ValueError(f"{param} must be > 0")
+
+        if value < 0 and param in ('x', 'y'):
+            raise ValueError(f"{param} must be >= 0")
