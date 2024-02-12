460590082a914c3af4f9301266a85d4303092787
chiemezie1 <chiemezieagbo1@gmail.com>
2024-02-12 16:44:53 +0100

    square   public getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,12 +14,34 @@ class Square(Rectangle):
         """
         initiating the attribute of the square from rectangle
         """
-        super.__init__(self, size, size, x, y, id)
+        super.__init__(size, size, x, y, id)
     
     def __str__(self):
         """
-        ...
+        Overloading __str__ method should return 
         """
+
         return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
             self.id, self.x, self.y, self.width
         )
+    
+    @property
+    def size(self):
+        """
+        A public getter method 
+        """
+        return self.__width
+    @size.setter
+    def size(self, value):
+        """
+        A public getter method 
+        """
+
+        self.__width = value
+        self.__height = value
+
+
+
+    
+
+    
