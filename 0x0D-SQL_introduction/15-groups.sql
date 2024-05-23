52ed7e0dee2e92ec33ed1942c4fecf27412f3dda
chiemezie1 <chiemezieagbo1@gmail.com>
2024-05-23 21:20:27 +0100

    16-no_link.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,13 @@
+-- A script that lists the number of records with the same score in
+--     the table second_table of the database in your MySQL server.
+
+-- The result display:
+--         the score
+--         the number of records for this score with the label number
+
+-- The list should be sorted by the number of records (descending)
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC
