c4d5668d7ff75b2faf897b345091ab9ebfcb3f4b
chiemezie1 <chiemezieagbo1@gmail.com>
2024-05-23 15:18:26 +0100

    6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- A script that lists all rows 
+--      of the table from the database in your MySQL server.
+
+-- The database name will be passed as an argument of the mysql command
+
+SELECT * FROM `first_table`;
