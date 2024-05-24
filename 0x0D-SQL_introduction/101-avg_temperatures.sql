4e9d238f78422684c468f4f215a2bea0fc56853c
chiemezie1 <chiemezieagbo1@gmail.com>
2024-05-24 11:19:24 +0100

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- This script calculates the average temperature in Fahrenheit by city
 -- and orders the results by temperature in descending order
 
-SELECT city, AVG(temperature) AS avg_temperature
+SELECT city, AVG(temperature) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temperature DESC;
