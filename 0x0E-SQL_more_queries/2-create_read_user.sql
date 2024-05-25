2700b92a95923b1ef402fac8b191c6975345587d
chiemezie1 <chiemezieagbo1@gmail.com>
2024-05-25 09:45:08 +0100

    ./2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- A script that creates the database hbtn_0d_2 and the user user_0d_2
+
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_
+-- user_0d_2 password -> user_0d_2_pwd
+
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
