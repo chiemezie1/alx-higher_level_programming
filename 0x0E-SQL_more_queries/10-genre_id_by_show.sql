bc5c17dc3b443f005f6fe6448a2be2412a16be22
chiemezie1 <chiemezieagbo1@gmail.com>
2024-05-25 20:17:29 +0100

    ./11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
-SELECT title, genre_id
-FROM hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
 WHERE tv_show_genres.genre_id NOT NULL
 ORDER BY tv_shows.title
