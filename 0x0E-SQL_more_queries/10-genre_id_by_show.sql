b1c95cbab60d07e4583c5e381a9b1141a3770bfb
chiemezie1 <chiemezieagbo1@gmail.com>
2024-05-25 20:21:46 +0100

    ./10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-WHERE tv_show_genres.genre_id NOT NULL
-ORDER BY tv_shows.title
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
