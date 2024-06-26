ee6947d04bf2a0b6ef00557ae5753966923b3d2c
chiemezie1 <chiemezieagbo1@gmail.com>
2024-06-26 17:16:45 +0100

    ./3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,7 +3,7 @@
 Script that takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
 
-Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password> <database name> <state name>
+Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <state name>
 """
 import sys
 import MySQLdb
