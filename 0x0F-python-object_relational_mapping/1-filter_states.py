b20d0378092071d53481435372a20f9b7902f548
chiemezie1 <chiemezieagbo1@gmail.com>
2024-06-26 13:15:39 +0100

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with 'N'
+    from the database hbtn_0e_0_ua
+
+Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>
+"""
+import sys
+import MySQLdb
+
+
+def list_n_states(username, password, database):
+    """
+    Connects to MySQL server and retrieves all states from 'states' table.
+
+    Args:
+    - username (str): MySQL username
+    - password (str): MySQL password
+    - database (str): Database name
+
+    Prints:
+    - State rows retrieved from the database
+    """
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            db=database,
+            host="localhost",
+            port=3306
+        )
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute the query
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        )
+        rows = cursor.fetchall()
+
+        # Print the rows
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL server: {e}")
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print(
+            "Usage: {} <mysql username> <mysql password> <database name>"
+            .format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_n_states(username, password, database)
