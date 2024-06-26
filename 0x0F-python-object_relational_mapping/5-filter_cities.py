344e039cb9292688662213a3d8684845be0005e4
chiemezie1 <chiemezieagbo1@gmail.com>
2024-06-26 20:44:31 +0100

    ./5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Script script that takes in the name of a state as an argument
+    and lists all cities of that state, using database hbtn_0e_4_usa
+
+"""
+import MySQLdb
+from sys import argv
+
+
+def filter_cities(username, password, database, state_name):
+    """
+    Filter cities by state name
+    """
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            db=database,
+            host="localhost",
+            port=3306
+        )
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute the query
+        cursor.execute(
+            """
+            SELECT cities.id, cities.name
+            FROM cities
+            JOIN states
+            ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC
+            """,
+            (state_name,)
+        )
+
+        # Print the rows
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL server: {e}")
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    filter_cities(username, password, database, state_name)
