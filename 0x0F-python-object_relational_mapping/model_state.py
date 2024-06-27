f41e8d40d2dd48e5578f1ccd3c6f423112857b94
chiemezie1 <chiemezieagbo1@gmail.com>
2024-06-27 14:56:20 +0100

    ./5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that defines a State class and creates a table in the database.
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class that inherits from Base
+    - Links to the MySQL table 'states'
+    - Has id and name attributes
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
