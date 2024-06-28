4c8e97682d0beefb79ad2fb87d19654d003dbc7f
chiemezie1 <chiemezieagbo1@gmail.com>
2024-06-28 16:59:24 +0100

    ./13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -8,12 +8,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("""Usage: ./13-model_state_delete_a.py
+        print(
+            """Usage: ./13-model_state_delete_a.py
               <mysql username> <mysql password> <database name>
-              """
-              )
+            """
+            )
         sys.exit(1)
 
     try:
