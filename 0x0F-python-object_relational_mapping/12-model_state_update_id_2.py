6f974b9c8735046e7427c58a7fa25095d1754b3a
chiemezie1 <chiemezieagbo1@gmail.com>
2024-06-28 16:23:37 +0100

    ./12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Script that changes the name of a State object from the database hbtn_0e_6_usa
-
-Usage: ./12-model_state_update_id_2.py
-    <mysql username> <mysql password> <database name> <state name>
+Script that updates the name of the State where id = 2 to New Mexico
 """
 import sys
 from sqlalchemy import create_engine
@@ -11,21 +8,38 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    """
-    Create engine to connect to MySQL database
-    """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    if len(sys.argv) != 4:
+        print("""Usage: ./update_state.py
+              <mysql username> <mysql password> <database name>
+              """
+              )
+        sys.exit(1)
 
-    Base.metadata.create_all(engine)
+    try:
+        # Create engine to connect to MySQL database
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        print("Database connection established.")
 
-    # Bind engine to session
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        # Create all tables in the engine
+        Base.metadata.create_all(engine)
 
-    # Change the name of the State where id = 2 to New Mexico
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = "New Mexico"
-    session.commit()
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    session.close()
+        # Query the State object with id = 2
+        state = session.query(State).filter(State.id == 2).first()
+
+        # Check if state exists
+        if state:
+            state.name = 'New Mexico'
+            session.commit()
+
+            print("State name updated to New Mexico.")
+        else:
+            print("State with id = 2 not found.")
+    except Exception as e:
+        print("Error: ", e)
+    finally:
+        # Close the session
+        session.close()
