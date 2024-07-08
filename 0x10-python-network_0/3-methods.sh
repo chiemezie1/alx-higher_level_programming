e64c78fe1f4e5f6913d36be7a863b24eea2da50c
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-08 12:07:45 +0100

    ./3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 # A Script  that takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f2 | sed 's/ //'
+curl -sI "$1" -X OPTIONS | grep "Allow:" | cut -d':' -f2 | sed 's/ //'
