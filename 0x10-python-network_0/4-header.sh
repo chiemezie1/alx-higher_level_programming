5f563e8358967c4265846b13d00b6423f4b56aae
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-08 13:40:21 +0100

    ./4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+# A Bash script that demonstrates how to send a GET request with JSON data using curl
 
-curl -s -X GET -H "content-type: application/json" -d '{"X-School-User-Id": "98"}' "$1" | cat -
+# Example URL (replace with your actual URL)
+URL="$1"
+
+# Example JSON data to send in the request body
+JSON_DATA='{"X-School-User-Id": "98"}'
+
+# Send the GET request with curl
+curl -s \
+     -X GET \
+     -H "Content-Type: application/json" \
+     -d "$JSON_DATA" \
+     "$URL"
