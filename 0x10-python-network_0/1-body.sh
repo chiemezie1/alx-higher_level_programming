6ab21d2fac21f8e3917b8fb04aa8f015b394351d
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-08 11:38:27 +0100

    ./1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This program takes in a URL, sends a GET request to the URL, and displays the body of the response.
-curl -sL "$1" -X GET -D ./response_header -o ./response_output; if grep -q "200 OK" ./header; then cat ./output; fi
-rm ./response_header ./response_output
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
+rm ./header ./output
