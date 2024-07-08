e1d0db4c3d7bc3278840eb5272131c5a46ee6c32
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-08 11:30:37 +0100

    ./1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays the body of a 200 status code response
 
-URL=$1
-
-# Send GET request and capture the HTTP status code
-HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-
-# If the HTTP status code is 200, then display the response body
-if [ "$HTTP_STATUS" -eq 200 ]; then
-    curl -s "$URL"
-fi
+curl -sL "$1" -X GET -D ./header -o ./body; if grep -q "200 OK" ./header; then cat ./output; fi
