a3d25ff32674997b91ae81cb9328811f04006c7b
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-08 13:43:02 +0100

    ./4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -L "$1" -X GET -H "X-School-User-Id": "98"
+# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the responses
+curl -s "$1" -X GET -H "X-School-User-Id": "98"
