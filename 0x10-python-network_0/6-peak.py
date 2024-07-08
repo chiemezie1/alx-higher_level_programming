766a66256fc1ddac7d333dd16d2ba40773be01b2
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-08 14:22:39 +0100

    peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,21 @@
+#!/usr/bin/python3
+"""
+A  function that finds a peak in a list of unsorted integers
+"""
 def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
-    """
+    """Find a peak element in a list of unsorted integers."""
     if not list_of_integers:
         return None
-    
-    low = 0
-    high = len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        
-        if list_of_integers[mid] >= list_of_integers[mid + 1]:
-            high = mid
+
+    def binary_search_peak(nums, left, right):
+        if left == right:
+            return nums[left]
+
+        mid = (left + right) // 2
+
+        if nums[mid] < nums[mid + 1]:
+            return binary_search_peak(nums, mid + 1, right)
         else:
-            low = mid + 1
-    
-    # At the end of the loop, low == high is the peak element
-    return list_of_integers[low]
+            return binary_search_peak(nums, left, mid)
+
+    return binary_search_peak(list_of_integers, 0, len(list_of_integers) - 1)
