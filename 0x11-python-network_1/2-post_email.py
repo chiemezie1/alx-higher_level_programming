56a2929dd8108c8107061f7d4327cc86934186da
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 08:52:49 +0100

    ./2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,8 @@
 Takes a URL and an Email and sends a POST request and email as a parameter to the URL
 """
 import sys
-import urllib.request
+from urllib import request, parse
+from sys import argv
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -11,7 +12,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     email = sys.argv[2]
-    data = urllib.parse.urlencode({"email": email}).encode("ascii")
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
+    data = parse.urlencode({"email": email}).encode("ascii")
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
