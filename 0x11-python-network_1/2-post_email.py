cadcab555d720ac3a4a0ae86be8c9ea77bd3b70c
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 08:57:30 +0100

    ./2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """"
-Takes a URL and an Email and sends a POST request and email as a parameter to the URL
+Takes a URL and an Email and sends a POST request
+    and email as a parameter to the URL
 """
 import sys
-from urllib import request, parse
-from sys import argv
+import urllib.request
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     email = sys.argv[2]
-    data = parse.urlencode({"email": email}).encode("ascii")
-    req = request.Request(sys.argv[1], data)
-    with request.urlopen(req) as response:
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
