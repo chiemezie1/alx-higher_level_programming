86c33063a9d2606005cbecddc79c4a43ac7895b3
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 09:12:24 +0100

    ./3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         print("Usage: {} <URL>".format(sys.argv[0]))
         sys.exit(1)
     else:
