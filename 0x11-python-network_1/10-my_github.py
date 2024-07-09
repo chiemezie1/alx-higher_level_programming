a1f9508d779119eaf17cfd1efa643d823ad15c86
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 11:17:31 +0100

    ./10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,8 +7,13 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/user"
     username = sys.argv[1]
-    password = sys.argv[2]
-    r = requests.get(url, auth=(username, password))
-    print(r.json().get('id'))
+    token = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(username, token))
+    if response.status_code == 200:
+        user_info = response.json()
+        print(user_info.get('id'))
+    else:
+        print(None)
