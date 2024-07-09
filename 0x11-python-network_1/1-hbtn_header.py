88057b69cfd739868cc9adef58607c07e0037962
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 08:09:36 +0100

    ./1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""
+"""
 A script that takes in URL, send a request to the URL and displays
     the value of the X-Request-Id variable in the response header
 """
@@ -9,4 +9,3 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.info()['X-Request-Id'])
-        response.close()
