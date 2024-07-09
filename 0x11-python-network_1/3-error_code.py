73a369b87afbe6e413eea9fc87d315ac403e9d31
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 09:13:53 +0100

    ./3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,15 +6,9 @@ Takes a URL and sends a request to the URL and displays the
 import urllib.request
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-    else:
-        try:
-            with urllib.request.urlopen(sys.argv[1]) as response:
-                print(response.read().decode("utf-8"))
-        except urllib.error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+try:
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.read().decode("utf-8"))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
 
-    sys.exit(0)
