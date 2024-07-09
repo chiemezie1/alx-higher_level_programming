c376a7fd41eb8881232ca9effa65d6592e36c976
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-09 10:04:39 +0100

    ./4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A script that fetches ALX intranet status using requests
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
