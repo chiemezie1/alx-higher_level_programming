7ef1e78faddd028dfcdbec4df58d1fa8e7eeb748
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-19 05:06:11 +0100

    ./2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const len = process.argv.length;
-if (len <= 2) {
+if (len <= 0) {
   console.log('No argument');
-} else if (len === 3) {
+} else if (len === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
