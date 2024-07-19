58d6c3480bd8e2d3356c63ca1bf876de4a219347
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-19 05:41:18 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const num = Number.isInteger(process.argv[2], 10);
-if (!isNaN(num)) {
-  console.log('My number: ' + num);
+const arg = process.argv[2];
+const num = Number.parseInt(arg, 10);
+
+if (!isNaN(num) && num.toString() === arg) {
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
