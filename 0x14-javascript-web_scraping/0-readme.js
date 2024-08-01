ce18c572fc09fa79efae1dff334af47b6c2adf29
chiemezie1 <chiemezieagbo1@gmail.com>
2024-08-01 04:41:10 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// A script that reads and prints the content of a file
+// reads a file
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    console.error(err);
+    return;
   }
-})
+  console.log(data);
+});
